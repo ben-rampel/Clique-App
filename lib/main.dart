f686609b9bc6dfe1f92acb01976ec94319c06188
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2019-10-12 11:39:25 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Clique());
+
+class Clique extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CliqueHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class CliqueHomePage extends StatefulWidget {
+  CliqueHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+     return _CliqueHomePageState();
+  }
+}
+
+class _CliqueHomePageState extends State<CliqueHomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("This is a test app")
+          ]
+    )));
+  }
+}
